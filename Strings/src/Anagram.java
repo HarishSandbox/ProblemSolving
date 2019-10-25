10d174d96133b28a0acddef75fff6d908b56d5eb
harish1992 <govindarajulu.h@husky.neu.edu>
2019-10-25 17:48:20 -0400

    +1 string op

--- Strings/src/Anagram.java
+++ Strings/src/Anagram.java
@@ -1,20 +1,40 @@
 public class Anagram {
 
   // All Permutation
-  public static char[] findAllAnagrams(String input) {
+  public void findAllAnagrams(String s,int l, int r) {
 
-    return null;
+    if(l==r){
+      System.out.println(s);
+      return;
+    }
+
+    for(int i =l ; i<=r; i++){
+      s = swap(s,l, i);
+      findAllAnagrams(s,l+1,r);
+      s = swap(s,l, i);  // undo swap so that the original string is used for next permutation
+    }
+
+  }
+
+  public String swap(String s,int i, int j ){
+    char[] arr = s.toCharArray();
+    char temp = arr[i];
+     arr[i] = arr[j];
+    arr[j] = temp;
+    return String.valueOf(arr);
   }
 
 
   //  When input has duplicates, Ex: "AAB"
-  public static char[] findAllDistinctAnagrams(String input) {
+  public char[] findAllDistinctAnagrams(String input) {
 
     return null;
   }
 
 
   public static void main(String args[]){
-    System.out.println(findAllAnagrams("abcd"));
+    Anagram obj = new Anagram();
+    String input = "abc";
+     obj.findAllAnagrams(input,0,input.length()-1);
   }
 }
